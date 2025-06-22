6953d366f189991e8da068eefe2f970c3c83525c
cris <est.cjvallev0201@uml.edu.ni>
2025-06-21 19:27:00 -0600

    Actualizar colores de la interfaz a aqua para una mejor coherencia visual

--- examen.py
+++ examen.py
@@ -14,11 +14,11 @@ root = tk.Tk()
 root.title("Ejercicio Primer parcial.Dania Silva")
 
 # Label centrado arriba
-label_nombre = tk.Label(root, text="Mi nombre es Dania Silva", bg="purple", fg="white", font=("Arial", 16, "bold"))
+label_nombre = tk.Label(root, text="Mi nombre es Dania Silva", bg="aqua", fg="white", font=("Arial", 16, "bold"))
 label_nombre.pack(pady=(60, 40), anchor="center")
 
 # Crear un frame para los botones (más abajo)
-frame_botones = tk.Frame(root, bg="purple")
+frame_botones = tk.Frame(root, bg="aqua")
 frame_botones.pack(pady=100)
 
 # Botón para mostrar la edad (izquierda)
@@ -33,7 +33,7 @@ boton_direccion.pack(side="right", padx=40)
 root.geometry("800x600")
 
 # Establecer un color de fondo
-root.configure(bg="purple")
+root.configure(bg="aqua")
 
 # Ejecutar el bucle principal de la ventana
 root.mainloop()
